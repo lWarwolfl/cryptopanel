f2bc8731752dd24a152ece68a07d3dd5e786395a
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2023-06-23 17:35:02 +0330

    Side Bar Mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,21 @@ import SideBar from "@/components/SideBar";
 import Table from "@/components/Table";
 import Head from "next/head";
 import * as React from "react";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import { IconButton, Button } from "@mui/material";
 
 export default function Home() {
+	const [isOpen, setIsOpen] = React.useState(false);
+
+	const handleMenuClick = () => {
+		setIsOpen(true);
+	};
+
+	const handleIconButtonClick = () => {
+		setIsOpen(false);
+	};
+
 	return (
 		<>
 			<Head>
@@ -13,8 +26,21 @@ export default function Home() {
 				<meta name="viewport" content="initial-scale=1, width=device-width" />
 			</Head>
 			<main className="main">
-				<div className="sidebar">
+				<div className={`sidebar ${isOpen ? "open" : ""}`}>
 					<SideBar />
+					<Button
+						className={`menu-button ${isOpen ? "hidden" : ""}`}
+						onClick={handleMenuClick}
+					>
+						Menu <ChevronRightRoundedIcon className="icon" />
+					</Button>
+
+					<IconButton
+						className={`menu-icon-button ${isOpen ? "" : "hidden"}`}
+						onClick={handleIconButtonClick}
+					>
+						<ChevronLeftRoundedIcon className="icon" />
+					</IconButton>
 				</div>
 				<div className="panel">
 					<Header />
