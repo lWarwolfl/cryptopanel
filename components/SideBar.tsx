f7d8f08d3cbed8472966c8ed8deb084fc1dd9cc1
Sina Kheiry <m.sinakheiry.warwolf@gmail.com>
2023-06-27 13:48:36 +0330

    Fix Side Bar Structure

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styles from "../styles/SideBar.module.scss";
-import { IconButton } from "@mui/material";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import { IconButton, Button } from "@mui/material";
 import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
 import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
 import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
@@ -16,72 +18,122 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TelegramIcon from "@mui/icons-material/Telegram";
 
 export default function SideBar() {
+	const sidebarRef = React.useRef<HTMLDivElement>(null);
+	const [isOpen, setIsOpen] = React.useState(false);
+
+	const handleMenuClick = (): void => {
+		setIsOpen(true);
+	};
+
+	const handleIconButtonClick = (): void => {
+		setIsOpen(false);
+	};
+
+	const handleOutsideClick = (event: MouseEvent | TouchEvent): void => {
+		if (
+			sidebarRef.current &&
+			!sidebarRef.current.contains(event.target as Node)
+		) {
+			setIsOpen(false);
+		}
+	};
+
+	React.useEffect(() => {
+		document.addEventListener("click", handleOutsideClick);
+		document.addEventListener("touchstart", handleOutsideClick);
+
+		return () => {
+			document.removeEventListener("click", handleOutsideClick);
+			document.removeEventListener("touchstart", handleOutsideClick);
+		};
+	}, []);
+
 	return (
 		<>
-			<div className={styles.sidebar}>
-				<div className={styles.logo}>crypto-panel-$</div>
+			<div
+				className={`${styles.sidebarcontainer} ${isOpen ? styles.open : null}`}
+				ref={sidebarRef}
+			>
+				<div className={styles.sidebar}>
+					<div className={styles.logo}>crypto-panel-$</div>
 
-				<div className={styles.seperator}></div>
+					<div className={styles.seperator}></div>
 
-				<div className={styles.title}>general</div>
-				<div className={styles.linkcontainer}>
-					<LightbulbOutlinedIcon className={styles.icon} />
-					<div className={styles.link}>Services</div>
-				</div>
-				<div className={styles.linkcontainer}>
-					<CreditCardOutlinedIcon className={styles.icon} />
-					<div className={styles.link}>Cards</div>
-				</div>
+					<div className={styles.title}>general</div>
+					<div className={styles.linkcontainer}>
+						<LightbulbOutlinedIcon className={styles.icon} />
+						<div className={styles.link}>Services</div>
+					</div>
+					<div className={styles.linkcontainer}>
+						<CreditCardOutlinedIcon className={styles.icon} />
+						<div className={styles.link}>Cards</div>
+					</div>
 
-				<div className={styles.title}>cryptocurrency</div>
-				<div className={`${styles.linkcontainer} ${styles.active}`}>
-					<StorefrontOutlinedIcon className={styles.icon} />
-					<div className={styles.link}>Marketplace</div>
-				</div>
-				<div className={styles.linkcontainer}>
-					<TollOutlinedIcon className={styles.icon} />
-					<div className={styles.link}>Trading</div>
-				</div>
-				<div className={styles.linkcontainer}>
-					<CandlestickChartRoundedIcon className={styles.icon} />
-					<div className={styles.link}>Statistics</div>
-				</div>
+					<div className={styles.title}>cryptocurrency</div>
+					<div className={`${styles.linkcontainer} ${styles.active}`}>
+						<StorefrontOutlinedIcon className={styles.icon} />
+						<div className={styles.link}>Marketplace</div>
+					</div>
+					<div className={styles.linkcontainer}>
+						<TollOutlinedIcon className={styles.icon} />
+						<div className={styles.link}>Trading</div>
+					</div>
+					<div className={styles.linkcontainer}>
+						<CandlestickChartRoundedIcon className={styles.icon} />
+						<div className={styles.link}>Statistics</div>
+					</div>
 
-				<div className={styles.title}>profile</div>
-				<div className={styles.linkcontainer}>
-					<HistoryToggleOffRoundedIcon className={styles.icon} />
-					<div className={styles.link}>History</div>
-				</div>
-				<div className={styles.linkcontainer}>
-					<TextsmsOutlinedIcon className={styles.icon} />
-					<div className={styles.link}>Messages</div>
-					<div className={styles.new}></div>
-				</div>
-				<div className={styles.linkcontainer}>
-					<SettingsOutlinedIcon className={styles.icon} />
-					<div className={styles.link}>Setting</div>
-				</div>
-				<div className={`${styles.linkcontainer} ${styles.nobottom}`}>
-					<SupportRoundedIcon className={styles.icon} />
-					<div className={styles.link}>Support</div>
-				</div>
+					<div className={styles.title}>profile</div>
+					<div className={styles.linkcontainer}>
+						<HistoryToggleOffRoundedIcon className={styles.icon} />
+						<div className={styles.link}>History</div>
+					</div>
+					<div className={styles.linkcontainer}>
+						<TextsmsOutlinedIcon className={styles.icon} />
+						<div className={styles.link}>Messages</div>
+						<div className={styles.new}></div>
+					</div>
+					<div className={styles.linkcontainer}>
+						<SettingsOutlinedIcon className={styles.icon} />
+						<div className={styles.link}>Setting</div>
+					</div>
+					<div className={`${styles.linkcontainer} ${styles.nobottom}`}>
+						<SupportRoundedIcon className={styles.icon} />
+						<div className={styles.link}>Support</div>
+					</div>
 
-				<div className={styles.seperator}></div>
+					<div className={styles.seperator}></div>
 
-				<div className={styles.socialcontainer}>
-					<IconButton className={styles.iconbutton}>
-						<InstagramIcon className={styles.icon} />
-					</IconButton>
-					<IconButton className={styles.iconbutton}>
-						<TwitterIcon className={styles.icon} />
-					</IconButton>
-					<IconButton className={styles.iconbutton}>
-						<FacebookIcon className={styles.icon} />
-					</IconButton>
-					<IconButton className={styles.iconbutton}>
-						<TelegramIcon className={styles.icon} />
-					</IconButton>
+					<div className={styles.socialcontainer}>
+						<IconButton className={styles.iconbutton}>
+							<InstagramIcon className={styles.icon} />
+						</IconButton>
+						<IconButton className={styles.iconbutton}>
+							<TwitterIcon className={styles.icon} />
+						</IconButton>
+						<IconButton className={styles.iconbutton}>
+							<FacebookIcon className={styles.icon} />
+						</IconButton>
+						<IconButton className={styles.iconbutton}>
+							<TelegramIcon className={styles.icon} />
+						</IconButton>
+					</div>
 				</div>
+				<Button
+					className={`${styles.menubutton} ${isOpen ? styles.hidden : null}`}
+					onClick={handleMenuClick}
+				>
+					Menu <ChevronRightRoundedIcon className={styles.icon} />
+				</Button>
+
+				<IconButton
+					className={`${styles.menuiconbutton} ${
+						isOpen ? null : styles.hidden
+					}`}
+					onClick={handleIconButtonClick}
+				>
+					<ChevronLeftRoundedIcon className={styles.icon} />
+				</IconButton>
 			</div>
 		</>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,8 @@ import SideBar from "@/components/SideBar";
 import Table from "@/components/Table";
 import Head from "next/head";
 import * as React from "react";
-import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
-import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
-import { IconButton, Button } from "@mui/material";
 
 export default function Home(): JSX.Element {
-	const sidebarRef = React.useRef<HTMLDivElement>(null);
-	const [isOpen, setIsOpen] = React.useState(false);
-
-	const handleMenuClick = (): void => {
-		setIsOpen(true);
-	};
-
-	const handleIconButtonClick = (): void => {
-		setIsOpen(false);
-	};
-
-	const handleOutsideClick = (event: MouseEvent | TouchEvent): void => {
-		if (
-			sidebarRef.current &&
-			!sidebarRef.current.contains(event.target as Node)
-		) {
-			setIsOpen(false);
-		}
-	};
-
-	React.useEffect(() => {
-		document.addEventListener("click", handleOutsideClick);
-		document.addEventListener("touchstart", handleOutsideClick);
-
-		return () => {
-			document.removeEventListener("click", handleOutsideClick);
-			document.removeEventListener("touchstart", handleOutsideClick);
-		};
-	}, []);
-
 	return (
 		<>
 			<Head>
@@ -46,22 +13,7 @@ export default function Home(): JSX.Element {
 				<meta name="viewport" content="initial-scale=1, width=device-width" />
 			</Head>
 			<main className="main">
-				<div className={`sidebar ${isOpen ? "open" : ""}`} ref={sidebarRef}>
-					<SideBar />
-					<Button
-						className={`menu-button ${isOpen ? "hidden" : ""}`}
-						onClick={handleMenuClick}
-					>
-						Menu <ChevronRightRoundedIcon className="icon" />
-					</Button>
-
-					<IconButton
-						className={`menu-icon-button ${isOpen ? "" : "hidden"}`}
-						onClick={handleIconButtonClick}
-					>
-						<ChevronLeftRoundedIcon className="icon" />
-					</IconButton>
-				</div>
+				<SideBar />
 				<div className="panel">
 					<Header />
 					<div className="analytics">
